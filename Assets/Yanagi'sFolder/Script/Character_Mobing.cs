cbff94dd996304fca4010fc5c7cee0c61a1287e6
yanagi121 <air_kent24@icloud.com>
2022-02-08 01:02:35 +0900

    キャラ移動修正

--- Assets/Yanagi'sFolder/Script/Character_Mobing.cs
+++ Assets/Yanagi'sFolder/Script/Character_Mobing.cs
@@ -6,6 +6,7 @@ public class Character_Mobing : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] float moveSpeed;
+    [SerializeField] bool CollisionGround;
     private Rigidbody rg;
     public float sensitivityX=1, sensitivityY=1,delay=2.0f;//マウスの感度設定標準設定で1
     [SerializeField] float X, Z,fallSpeed,Y;
@@ -20,7 +21,7 @@ public class Character_Mobing : MonoBehaviour
        // cameraX = transCamera.localEulerAngles.x;
         cameraX=0;
         Invoke("Progress", delay);
-        Physics.gravity = new Vector3(0,-9.8f,0);
+      //Physics.gravity = new Vector3(0,fallSpeed, 0);
 
     }
 
@@ -56,25 +57,47 @@ public class Character_Mobing : MonoBehaviour
     void FixedUpdate()//地面との当たり判定を用いて、自由落下を作る
     {
         if (TimeProgress==true) {
-            if (moveZ || moveX || mouseXbool || mouseYbool)
+            if ((moveZ || moveX || mouseXbool || mouseYbool)&& CollisionGround)
             {
                 moveZ = false;
                 moveX = false;
                 mouseXbool = false;
                 mouseYbool = false;
                
-                rg.velocity = transform.rotation * new Vector3(X, Physics.gravity.y, Z) * moveSpeed * Time.deltaTime * 100;
+                rg.velocity = transform.rotation * new Vector3(X, 0, Z) * moveSpeed * Time.deltaTime * 100;
                 //rg.AddForce(X*moveSpeed, 0, Z* moveSpeed);
                 rg.MoveRotation(Quaternion.Euler(0.0f, rg.rotation.eulerAngles.y + mouseX * Time.deltaTime * 100.0f, 0.0f));
                 cameraX = Mathf.Clamp(cameraX - mouseY * Time.deltaTime * 100.0f, -40, 40);
                 transCamera.localEulerAngles = new Vector3(cameraX, 0, 0);
                 //rg.velocity = transform.rotation * new Vector3(X * moveSpeed, 0, Z * moveSpeed) * Time.deltaTime * 100;
             }
-            else 
+            else if((moveZ || moveX || mouseXbool || mouseYbool) && CollisionGround==false)
             {
-               // rg.velocity =new Vector3(0,Y);
-            } 
+                fallSpeed -= Y;
+                rg.velocity = transform.rotation * new Vector3(X, fallSpeed, Z) * moveSpeed * Time.deltaTime * 100;
+                //rg.AddForce(X*moveSpeed, 0, Z* moveSpeed);
+                rg.MoveRotation(Quaternion.Euler(0.0f, rg.rotation.eulerAngles.y + mouseX * Time.deltaTime * 100.0f, 0.0f));
+                cameraX = Mathf.Clamp(cameraX - mouseY * Time.deltaTime * 100.0f, -40, 40);
+                transCamera.localEulerAngles = new Vector3(cameraX, 0, 0);
+                
+
+            } else
+            {
+                // rg.velocity =new Vector3(0,Y);
+            }
         }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        CollisionGround=false;
+        
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        CollisionGround=true;
+       
+            fallSpeed = Y;
+
     }
     void Progress()
     {
