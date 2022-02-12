9f4f70f2152f7e790a2f81feadd9c752c012b2af
OhoriAoi <cnwk329@gmail.com>
2022-02-12 10:47:18 +0900

    Drの修正

--- Assets/Ohori/New Folder/Script/Dr_InvisibleControl.cs
+++ Assets/Ohori/New Folder/Script/Dr_InvisibleControl.cs
@@ -14,14 +14,10 @@ public class Dr_InvisibleControl : MonoBehaviour
     private float EscapeSpeed = 8.0f;//逃げる速さ
 
     //Playerを宣言
-    [SerializeField]
-    public GameObject Player1;
-    [SerializeField]
-    public GameObject Player2;
-    [SerializeField]
-    public GameObject Player3;
-    [SerializeField]
-    public GameObject Player4;
+    private GameObject Player1;
+    private GameObject Player2;
+    private GameObject Player3;
+    private GameObject Player4;
 
     //Playerの位置を入れる変数
     [SerializeField]
@@ -47,11 +43,11 @@ public class Dr_InvisibleControl : MonoBehaviour
 
     void Start()
     {
+        //プレイヤーを見つける
+        findPlayer();
+
         //プレイヤーとの距離を初期化
-        toPlayer1Distance = new Vector3(0, 0, 0);
-        toPlayer2Distance = new Vector3(0, 0, 0);
-        toPlayer3Distance = new Vector3(0, 0, 0);
-        toPlayer4Distance = new Vector3(0, 0, 0);
+        initializeToPlayerDistance();
 
         // NavMeshAgentコンポーネントを取得
         m_navMeshAgent = GetComponent<NavMeshAgent>();
@@ -61,18 +57,48 @@ public class Dr_InvisibleControl : MonoBehaviour
     void Update()
     {
         //Playerの位置を入れる
+        setPlayerPos();
+
+        //プレイヤーとの距離を入れる
+        setToPlayerDistance();
+
+        // NavMeshが準備できているなら
+        if (m_navMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid) { m_navMeshAgent.SetDestination(TargetObject.transform.position); }// NavMeshAgentに目的地をセット
+    }
+
+    //プレイヤーを見つけるメソッド    
+    public void findPlayer()
+    {
+        Player1 = GameObject.Find("Player1");
+        Player2 = GameObject.Find("Player2");
+        Player3 = GameObject.Find("Player3");
+        Player4 = GameObject.Find("Player4");
+    }
+
+    //プレイヤーとの距離を初期化するメソッド
+    public void initializeToPlayerDistance()
+    {
+        toPlayer1Distance = new Vector3(0, 0, 0);
+        toPlayer2Distance = new Vector3(0, 0, 0);
+        toPlayer3Distance = new Vector3(0, 0, 0);
+        toPlayer4Distance = new Vector3(0, 0, 0);
+    }
+
+    //Playerの位置を入れるメソッド
+    public void setPlayerPos()
+    {
         Player1Pos = Player1.transform.position;
         Player2Pos = Player2.transform.position;
         Player3Pos = Player3.transform.position;
         Player4Pos = Player4.transform.position;
+    }
 
-        //プレイヤーとの距離を入れる
+    //プレイヤーとの距離を入れるメソッド
+    public void setToPlayerDistance()
+    {
         if (movePointControl.Player1In == true) { toPlayer1Distance = Player1Pos - transform.position; }
         if (movePointControl.Player2In == true) { toPlayer2Distance = Player2Pos - transform.position; }
         if (movePointControl.Player3In == true) { toPlayer3Distance = Player3Pos - transform.position; }
         if (movePointControl.Player4In == true) { toPlayer4Distance = Player4Pos - transform.position; }
-
-        // NavMeshが準備できているなら
-        if (m_navMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid) { m_navMeshAgent.SetDestination(TargetObject.transform.position); }// NavMeshAgentに目的地をセット
     }
 }
