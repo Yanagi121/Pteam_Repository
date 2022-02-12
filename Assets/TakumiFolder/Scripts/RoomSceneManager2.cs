51c2520aa69ea7ac20aa1fee990e835b465bce52
SatoTaku7 <takumisato910@gmail.com>
2022-02-12 12:30:10 +0900

    テストシーンのスクリプト修正・博士の見た目変更をしました

--- Assets/TakumiFolder/Scripts/RoomSceneManager2.cs
+++ Assets/TakumiFolder/Scripts/RoomSceneManager2.cs
@@ -64,10 +64,19 @@ public class RoomSceneManager2 : MonoBehaviourPunCallbacks
         Debug.Log("待機ルームに参加");
         //  LobbyUI.SetActive(false);
         //  enterMatchWaitRoomUI.SetActive(true);
-        GameObject player = PhotonNetwork.Instantiate("Player", new Vector3(176, 30, 262), Quaternion.identity) as GameObject;
-        player.name = "PlayerPrefab";
-
-        Debug.Log("これはUserのid:");
+        //GameObject player = PhotonNetwork.Instantiate("Player", new Vector3(176, 30, 262), Quaternion.identity) as GameObject;
+        ///////////////////////////////////////////////////////////絶対修正
+        //if (PhotonNetwork.IsMasterClient)
+        //{
+        //    Debug.Log("自身がマスタークライアントです");
+        //    player.name = "Player" + 1;
+        //}
+        //else
+        //{
+        //    Debug.Log("自身がローカルプライヤーです");
+        //    player.name = "Player" + 2;
+        //}
+        /////////////////////////////////////////////////////////////
         // PhotonNetwork.Instantiate("Avatar", new Vector3(0, 0, 0), Quaternion.identity);//他のネットワークオブジェクトがダメだったので可視化されているこれで確認したが駄目だった
         //Debug.Log("ルームに入ってない人"+PhotonNetwork.CountOfPlayersOnMaster);　未解決
         //Debug.Log("ルームに入っている人" + PhotonNetwork.CountOfPlayersInRooms); 未解決
