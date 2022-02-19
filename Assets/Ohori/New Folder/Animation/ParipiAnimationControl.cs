9e38247476ff4f5eeae9a7290c05e3e9dc85ed52
OhoriAoi <cnwk329@gmail.com>
2022-02-19 16:19:26 +0900

    アニメーションも必要なものの追加

---
+++ Assets/Ohori/New Folder/Animation/ParipiAnimationControl.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class ParipiAnimationControl : MonoBehaviour
+{
+    Animator animator;
+
+    [SerializeField]
+    public bool runControl;//走るかどうかを決める変数
+
+
+    void Start()
+    {
+        // Animatorコンポーネントを取得
+        animator = GetComponent<Animator>();
+
+        //runControlの初期化
+        runControl = false;
+    }
+
+    void Update()
+    {
+        //ParipiAnimationControlないのrunControlがtrueかfalseかの判定
+        MoveJudgment();
+
+        //AnimatorないのrunControlのtrue/falseの判定
+        animator.SetBool("runControl", runControl);
+    }
+
+    //ParipiAnimationControlないのrunControlがtrueかfalseかの判定するメソッド
+    public void MoveJudgment()
+    {
+        if (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")){ runControl = true;}
+        else{runControl = false; }
+        if (Input.GetKey("up") || Input.GetKey("left") || Input.GetKey("down") || Input.GetKey("right")) { runControl = true; }
+        else { runControl = false; }
+    }
+}
