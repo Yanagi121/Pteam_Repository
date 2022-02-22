782d1ce2181596edb728ef63504e6b5ddb2a4d51
yanagi121 <air_kent24@icloud.com>
2022-02-23 00:45:58 +0900

    スクリプト追加

--- Assets/Yanagi'sFolder/Script/TestScript/LockCursor.cs
+++ Assets/Yanagi'sFolder/Script/TestScript/LockCursor.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LockCursor : MonoBehaviour
 {
+    public GameObject SoundSettingsUIObject;
+    
+    public static bool OnClickEscape;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +19,26 @@ public class LockCursor : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
+            /*  Cursor.visible = true;
+              Cursor.lockState = CursorLockMode.None;*/
+            if (OnClickEscape)
+            {
+                OnClickEscape = false;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                SoundSettingsUIObject.SetActive(true);
+               
+            }
+            else
+            {
+                OnClickEscape = true;
+               
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                SoundSettingsUIObject.SetActive(false);
+               
+            }
         }
+      
     }
 }
