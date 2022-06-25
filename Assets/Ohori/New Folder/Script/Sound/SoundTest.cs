95d5449d7b55ae97b94cd770fa26b13dce0adace
OhoriAoi <cnwk329@gmail.com>
2022-06-25 22:25:25 +0900

    音量設定

--- Assets/Ohori/New Folder/Script/Sound/SoundTest.cs
+++ Assets/Ohori/New Folder/Script/Sound/SoundTest.cs
@@ -14,8 +14,9 @@ public class SoundTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
+            soundManager.PlaySeByName("hito_ge_aru_mizu02");
             soundManager.PlayBgmByName("game_2");
             Debug.Log("çƒê∂");
         }
