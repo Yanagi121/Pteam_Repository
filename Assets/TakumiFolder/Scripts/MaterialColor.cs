27cbb7d7d756578c534681b4a291d57e2af59ebe
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-08-21 23:24:31 +0900

    待機画面の背景の変更・プレイ時にプレイヤー自身が見えなくなる機能の試し実装

---
+++ Assets/TakumiFolder/Scripts/MaterialColor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MaterialColor : MonoBehaviour
+{
+    private bool onetime;
+    SkinnedMeshRenderer mesh;
+    void Start()
+    {
+        mesh = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "TestPlayScene")
+        {
+            //mesh.enabled = false;
+            onetime = false;
+
+        }
+    }
+}
