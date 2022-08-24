7e55196e5139c1ca3bb81f58dfe1877867368f3b
DESKTOP-AJ616QO\chino <air_kent24@icloud.com>
2022-08-24 23:11:51 +0900

    ミニマップの作製（簡易版）

--- Assets/Yanagi'sFolder/Script/Character_Moving.cs
+++ Assets/Yanagi'sFolder/Script/Character_Moving.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character_Mobing : MonoBehaviour
+public class Character_Moving : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] float moveSpeed;
