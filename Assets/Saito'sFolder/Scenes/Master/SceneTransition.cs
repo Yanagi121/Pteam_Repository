649d547265b1c138dbb6cc4c1472280e95932d20
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-09-09 18:47:24 +0900

    齋藤さんのフォルダーを追加(2)

---
+++ Assets/Saito'sFolder/Scenes/Master/SceneTransition.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SceneTransition : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField] private SceneAsset _scene;
+
+    public void SetDestSceneName()
+    {
+        destSceneName = _scene.name;
+    }
+
+#endif
+
+    [SerializeField] string destSceneName;
+
+    void Start()
+    {
+#if UNITY_EDITOR
+        this.GetComponent<Button>().OnClickAsObservable()
+            .Subscribe(_ => SceneManager.LoadScene(_scene.name)).AddTo(this);
+
+#else
+ this.GetComponent<Button>().OnClickAsObservable()
+            .Subscribe(_ => SceneManager.LoadScene(destSceneName)).AddTo(this);
+
+#endif
+    }
+}
