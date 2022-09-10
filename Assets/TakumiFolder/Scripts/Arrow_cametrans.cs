7e2a7b683a5ffe3c6c25f0712de247f99824f483
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-09-10 21:37:55 +0900

    矢印の機能を改善

--- Assets/TakumiFolder/Scripts/Arrow_cametrans.cs
+++ Assets/TakumiFolder/Scripts/Arrow_cametrans.cs
@@ -1,19 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Arrow_cametrans : MonoBehaviour
 {
-    [SerializeField] GameObject Arrow;
+    //[SerializeField] GameObject Arrow;
 
     void Start()
     {
+        
+            switch (DontDestroy_Porder.Porder_handover)
+            {
+                case 1: GameObject.FindGameObjectWithTag("ArrowCam1").SetActive(true); if_Cam(DontDestroy_Porder.Porder_handover); Debug.Log(DontDestroy_Porder.Porder_handover); break;
+                case 2: GameObject.FindGameObjectWithTag("ArrowCam2").SetActive(true); if_Cam(DontDestroy_Porder.Porder_handover); Debug.Log(DontDestroy_Porder.Porder_handover); break;
+                case 3: GameObject.FindGameObjectWithTag("ArrowCam3").SetActive(true); if_Cam(DontDestroy_Porder.Porder_handover); Debug.Log(DontDestroy_Porder.Porder_handover); break;
+                case 4: GameObject.FindGameObjectWithTag("ArrowCam4").SetActive(true); if_Cam(DontDestroy_Porder.Porder_handover); Debug.Log(DontDestroy_Porder.Porder_handover); break;
+                default: Debug.Log(DontDestroy_Porder.Porder_handover); break;
+            }
         
     }
 
    
     void Update()
     {
-        this.transform.position = Arrow.transform.position+new Vector3(0,15,-10);
+        
+    }
+    void if_Cam(int playernum)//ÉJÉÅÉâÇ™Ç†ÇÈÇ©í≤Ç◊ÅAÇ†Ç¡ÇΩÇÁè¡Ç∑
+    {
+        switch (playernum)
+        {
+            case 1:
+                if (GameObject.FindGameObjectWithTag("ArrowCam2"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam2").SetActive(false);
+                }
+                if (GameObject.FindGameObjectWithTag("ArrowCam3"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam3").SetActive(false);
+                }
+                if (GameObject.FindGameObjectWithTag("ArrowCam4"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam4").SetActive(false);
+                }
+                break;
+            case 2:
+                if (GameObject.FindGameObjectWithTag("ArrowCam1"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam1").SetActive(false);
+                }
+                if (GameObject.FindGameObjectWithTag("ArrowCam3"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam3").SetActive(false);
+                }
+                if (GameObject.FindGameObjectWithTag("ArrowCam4"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam4").SetActive(false);
+                }
+                break;
+            case 3:
+                if (GameObject.FindGameObjectWithTag("ArrowCam2"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam2").SetActive(false);
+                }
+                if (GameObject.FindGameObjectWithTag("ArrowCam1"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam1").SetActive(false);
+                }
+                if (GameObject.FindGameObjectWithTag("ArrowCam4"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam4").SetActive(false);
+                }
+                break;
+            case 4:
+                if (GameObject.FindGameObjectWithTag("ArrowCam2"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam2").SetActive(false);
+                }
+                if (GameObject.FindGameObjectWithTag("ArrowCam3"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam3").SetActive(false);
+                }
+                if (GameObject.FindGameObjectWithTag("ArrowCam1"))
+                {
+                    GameObject.FindGameObjectWithTag("ArrowCam1").SetActive(false);
+                }
+                break;
+        }
+        
     }
 }
--- Assets/Yanagi'sFolder/Script/WallCollisionDetection.cs
+++ Assets/Yanagi'sFolder/Script/WallCollisionDetection.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WallCollisionDetection : MonoBehaviour { 
     GameObject wall;
     GameObject StageObject;
     public int ColDirectionNum;
     public bool ObjectCol=false;
+    [SerializeField] GameObject Arrow;
+    bool onetime;
     public enum ColDirection
     {
         NONE=0, //0
@@ -16,7 +19,20 @@ public class WallCollisionDetection : MonoBehaviour {
         LEFT=4, //4
     }
     public static ColDirection col=(ColDirection)0;
-    
+
+    private void Start()
+    {
+        onetime = true;
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "TestPlayScene"&&onetime==true)
+        {
+            Arrow = GameObject.FindGameObjectWithTag("Arrow");
+            onetime = false;
+        }
+    }
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("wall") || other.gameObject.CompareTag("StageObject"))
@@ -24,7 +40,9 @@ public class WallCollisionDetection : MonoBehaviour {
             ObjectCol = true;
             col = (ColDirection)ColDirectionNum;
         }
-    }
+        if (other.gameObject.tag == "Arrow_col") Arrow.SetActive(false);
+    
+}
     void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("wall")||other.gameObject.CompareTag("StageObject"))
@@ -32,5 +50,6 @@ public class WallCollisionDetection : MonoBehaviour {
             ObjectCol = false;
             col = (ColDirection)0;
         }
+        if (other.gameObject.tag == "Arrow_col") Arrow.SetActive(true);
     }
 }
