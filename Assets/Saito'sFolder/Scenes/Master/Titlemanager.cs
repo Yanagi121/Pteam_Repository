08685ebcf168934a3e36e4605ab6c9775f0f0d0b
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-09-11 16:01:35 +0900

    タイトルシーンの実装を行いました

---
+++ Assets/Saito'sFolder/Scenes/Master/Titlemanager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Titlemanager : MonoBehaviour
+{
+    [SerializeField] private GameObject panel; 
+    
+    private void Start()
+    {
+        waitTest(this.GetCancellationTokenOnDestroy()).Forget();
+    }
+
+    async UniTask waitTest(CancellationToken token)  
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(5f),cancellationToken: token);  
+        panel.gameObject.SetActive(false);
+        
+        
+          Observable.EveryUpdate()
+                    .Where(_ => Input.anyKey)
+                    .Subscribe(_ => SceneManager.LoadScene("Saito'sFolder/Scenes/Master/Main"))
+                    .AddTo(this);
+    }
+}
