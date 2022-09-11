b3ce21ed4fd252a91a3150d16b379fdc0a334829
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-09-12 04:01:58 +0900

    オプションのスクリプト機能面を追加しました。

--- Assets/Saito'sFolder/Scripts/OptionManager.cs
+++ Assets/Saito'sFolder/Scripts/OptionManager.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class OptionManager : MonoBehaviour
+namespace Saito
 {
-    // Start is called before the first frame update
-    void Start()
+    public class OptionManager : MonoBehaviour
     {
-        
-    }
+        //button
+        [SerializeField] private Button _soundButton;
+        [SerializeField] private Button _creditButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //panel
+        [SerializeField] private GameObject _soundpanel;
+        [SerializeField] private GameObject _creditpanel;
+
+        //image
+        [SerializeField] private Sprite _soundAciveImage;
+        [SerializeField] private Sprite _soundNotAciveImage;
+        [SerializeField] private Sprite _creditAciveImage;
+        [SerializeField] private Sprite _creditNotAciveImage;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _soundButton.OnClickAsObservable()
+                .Select(_ => _creditpanel.gameObject.activeSelf)
+                .Subscribe(value =>
+                {
+                    if (value)
+                    {
+                        _creditButton.image.sprite = _creditNotAciveImage;
+                        _creditpanel.gameObject.SetActive(false);
+                    }
+                    _soundButton.image.sprite = _soundAciveImage;
+                    _soundpanel.gameObject.SetActive(true);
+                }).AddTo(this);
+
+            _creditButton.OnClickAsObservable()
+                .Select(_ => _soundpanel.gameObject.activeSelf)
+                .Subscribe(value =>
+                {
+                    if (value)
+                    {
+                        _soundButton.image.sprite = _soundNotAciveImage;
+                        _soundpanel.gameObject.SetActive(false);
+                    }
+
+                    _creditButton.image.sprite = _creditAciveImage;
+                    _creditpanel.gameObject.SetActive(true);
+                }).AddTo(this);
+        }
     }
 }
