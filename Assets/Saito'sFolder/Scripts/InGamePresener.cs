81139fbf0b68f8d2ec421383be6c1d90e0612e8e
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-10-15 04:54:36 +0900

    フォントの追加とスクリプトの追加

---
+++ Assets/Saito'sFolder/Scripts/InGamePresener.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGamePresener : MonoBehaviour
+{
+    [SerializeField] private inGameModel _model;
+    [SerializeField] private InGameView _view;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Saito'sFolder/Scripts/InGameView.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InGameView : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro _text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
