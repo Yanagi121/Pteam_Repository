a050532c8920ef3e6a4041bf30f085a476dcbf31
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2023-01-03 07:59:28 +0900

    feat：helpシーンのスライド移動機能を追加

--- Assets/Saito'sFolder/Scripts/Help/HelpModel.cs
+++ Assets/Saito'sFolder/Scripts/Help/HelpModel.cs
@@ -1,17 +1,25 @@
 using UniRx;
+using UnityEngine;
 
 namespace Help.Saito
 {
     public class HelpModel
     {
         private readonly BoolReactiveProperty _isPushProp;
-
+        private readonly FloatReactiveProperty _pageCurrentNumProp;
+        
         /// <summary>
         /// ボタンが押されたか
         /// </summary>
         public IReactiveProperty<bool> IsPushProp => _isPushProp;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public IReactiveProperty<float> PageCurrentNumProp => _pageCurrentNumProp;
+
         public bool IsPush => _isPushProp.Value;
+        public float PageCurrentNum=>_pageCurrentNumProp.Value;
         
         /// <summary>
         /// コンストラクタ
@@ -19,6 +27,7 @@ namespace Help.Saito
         public HelpModel()
         {
             _isPushProp = new BoolReactiveProperty(false);
+            _pageCurrentNumProp = new FloatReactiveProperty(0.0f);
         }
 
         /// <summary>
@@ -27,6 +36,14 @@ namespace Help.Saito
         public void UpdateBool(bool isPush)
         {
             _isPushProp.Value = isPush;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public void UpdatePageNum(float pageNextNum)
+        {
+            _pageCurrentNumProp.Value = Mathf.Clamp(pageNextNum,0.0f,2.0f);
         }
     }
 }
